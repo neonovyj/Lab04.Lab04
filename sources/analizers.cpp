460743019431adbe2fd79c3a71bbc678537bbc6b
neonovyj <ikhonyak@gmail.com>
2021-01-20 22:23:57 +0300

    v2

--- sources/analizers.cpp
+++ sources/analizers.cpp
@@ -6,29 +6,29 @@
 #include <boost/filesystem.hpp>
 #include <string>
 #include <vector>
-
-inline bool is_separator(char c) { return c == '_'; }
-inline bool not_separator(char c) { return !is_separator(c); }
-std::vector<std::string> split_string(const std::string &text) {
-  std::vector<std::string> ret;
-  for (auto i = text.cbegin(); i != text.cend();) {
-    i = std::find_if(i, text.end(), not_separator);
-    auto j = std::find_if(i, text.end(), is_separator);
-    ret.push_back(std::string(i, j));
+//is_separator not_separator объявлены в качестве вспомогательных функций. inline - слово, говорящее компилятору, что эту функцию надо не вызывать, а подставлять ее текст. Если компилятор решит, что так выгоднее с точки зрения производительности
+inline bool is_separator(char c) { return c == '_'; }//функция, которая вернет тру, если символ является разделителем
+inline bool not_separator(char c) { return !is_separator(c); }//функция, которая вохвращает тру, если текщий символ не является разделителм
+std::vector<std::string> split_string(const std::string &text) { //функция, которая по нужному разделителю разбивает строки на подстроки
+  std::vector<std::string> ret; //у нас есть массив строк. Выполняет список подстрок
+  for (auto i = text.cbegin(); i != text.cend();) { //проходим по строке итерируя ее специальными итераторами, которые у нее есть
+    i = std::find_if(i, text.end(), not_separator); //find if в некотором множестве ищет такой элемент, для которого будет выполняться такое условие. будет искать такой символ, для которого not_separator будет тру(найдет символ, который не является разделителем)
+    auto j = std::find_if(i, text.end(), is_separator); //i, j -итераторы строки
+    ret.push_back(std::string(i, j)); //std::string-создаем строку из итераторов, которые мы нашли
     i = j;
   }
   return ret;
 }
-
-void processing_filename(const std::string &filename,
-                         std::vector<cash_file> &files,
-                         std::set<std::string> &accounts) {
-  auto filename_tokens = split_string(filename);
-  if (filename_tokens.size() != 3) return;
-
-  auto type = filename_tokens[0];
-  auto account = filename_tokens[1];
-  auto date = filename_tokens[2];
+//ниже обрабатываем имя. Распарсивает имя, вытаскивает его номер и вставила в множество аккаунтов. Делается множество, чтобы каждый элемент был уникален
+void processing_filename(const std::string &filename,//первым аргументом принимает имя. полное имя файла
+                         std::vector<cash_file> &files,//ссылка на массив финансовых файлов
+                         std::set<std::string> &accounts) {//ссылка на множество аккаунтов
+  auto filename_tokens = split_string(filename);//std vector string (auto сам выводит тип, но мы знаем, что split_string возвращает вектор строк)
+  if (filename_tokens.size() != 3) return; //если элементов файла не три, то файл игнорируется
+//Разбиваем имя аккаунта на 3 элемента, потом проверяем на валидность(нужен ли нам этот файл)
+  auto type = filename_tokens[0];//std string
+  auto account = filename_tokens[1];//std string
+  auto date = filename_tokens[2];//std string
 
   if (type != "balance") return;
   if (account.length() != 8) return;
