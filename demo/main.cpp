5a2e0c4f70be474876b6f85a20243dc3e4af2d68
neonovyj <ikhonyak@gmail.com>
2021-01-21 16:14:31 +0300

    v5

--- demo/main.cpp
+++ demo/main.cpp
@@ -13,7 +13,7 @@ std::string lastdate(const std::vector<cash_file> &files) {
   return res;
 }
 
-void print_files(const std::vector<broker> &brokers) {
+void print_files(const std::vector<broker> &brokers) { //для каждого брокера в цикле выводим его файлы
   for (auto const &broker : brokers) {
     for (auto const &broker_file : broker.files()) {
       std::cout << broker.name() << " " << broker_file.name() << std::endl;
@@ -22,11 +22,11 @@ void print_files(const std::vector<broker> &brokers) {
 }
 
 void short_print_files(const std::vector<broker> &brokers) {
-  for (auto const &broker : brokers) {
-    for (auto const &account : broker.accounts()) {
-      std::vector<cash_file> this_account_files;
-      for (auto const &file : broker.files()) {
-        if (file.account() == account) this_account_files.push_back(file);
+  for (auto const &broker : brokers) {//перебираем брокеров
+    for (auto const &account : broker.accounts()) {//перебираем аккаунты каждого брокера
+      std::vector<cash_file> this_account_files;//вектор с файлами
+      for (auto const &file : broker.files()) {//перебираем файлы
+        if (file.account() == account) this_account_files.push_back(file);//записываем кол-во файлов
       }
       std::cout << "broker:" << broker.name() << " account:" << account
                 << " files:" << this_account_files.size()
@@ -35,17 +35,17 @@ void short_print_files(const std::vector<broker> &brokers) {
   }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {//argc argv - аргументы командной строки(argcount argvalue)
   std::string directory;
-  if (argc == 1) {
-    directory = ".";
+  if (argc == 1) { //1 потому что первый аргумент- название самой программы
+    directory = ".";//если не передается никакого аргумета, то в качестве него передается то, что ниже(директория, в которой находимся)
   } else {
-    directory = argv[1];
+    directory = argv[1];//иначе мы берем второй аргумент
   }
   directory = "../misc/ftp";
 
-  std::vector<broker> brokers = analyse_all(directory);
-  print_files(brokers);
+  std::vector<broker> brokers = analyse_all(directory);//получаем список всех брокеров
+  print_files(brokers); //передаем их фунции для вывода
   short_print_files(brokers);
   return 0;
 }
