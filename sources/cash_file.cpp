5ab9acb631fd5be89057c569474a4fdee2f71f7f
neonovyj <ikhonyak@gmail.com>
2021-01-23 19:29:29 +0300

    v11

--- sources/cash_file.cpp
+++ sources/cash_file.cpp
@@ -2,7 +2,7 @@
 #include <cash_file.hpp>
 cash_file::cash_file(const std::string& name, const std::string& date,
                                const std::string& account)
-    : _name(name), _date(date), _account(account) {}
+    : _name(name), _date(date), _account(account) {}//определяем конструктор от параметров(имя и дата)
 
 std::string cash_file::name() const { return _name + ".txt"; }//тут name не имя брокера, а имя файла/вернет строку имени. Конст означает, что метод не меняет полей класса
 
